395c4fa86c0c49cd87f9269672b605f20dae2674
Tishamahale25 <Tishamahale25@gmail.com>
2025-04-29 16:38:01 +0530

    save master

--- addition.java
+++ addition.java
@@ -4,7 +4,8 @@ public class addition {
     public static void main(String[] args) {
         int a=10;
         int b=23;
-        int sum=a+b;
+        int c=25;
+        int sum=a+b+c;
         System.out.println(sum);
     }
 }
