4554dd8da4a38c04f22a43e333f53fd3739efc84
Tishamahale25 <tishamahale25@gmail.com>
2025-04-29 16:34:09 +0530

    save

---
+++ addition.java
@@ -0,0 +1,10 @@
+package addition;
+
+public class addition {
+    public static void main(String[] args) {
+        int a=10;
+        int b=23;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
